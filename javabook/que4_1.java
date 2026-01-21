6bde66db1d3415ad731d01e8481b0904cdf08cc9
Sanskar Sinha <sanskarsinhanew@gmail.com>
2026-01-21 20:06:32 +0530

    varible,datatypes

---
+++ javabook/que4_1.java
@@ -0,0 +1,12 @@
+package javabook;
+public class que4_1 {
+    public static void main(String args[]){
+        int number =50;
+        byte num=5;
+        System.out.println(number);
+        System.out.println(num);
+
+    }
+
+    
+}
