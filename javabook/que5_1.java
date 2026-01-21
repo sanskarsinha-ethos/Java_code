d9d69223ad283a1247f4799c7e7cbb1fb236b347
Sanskar Sinha <sanskarsinhanew@gmail.com>
2026-01-21 22:03:22 +0530

    basics

---
+++ javabook/que5_1.java
@@ -0,0 +1,15 @@
+package javabook;
+import java.util.*;
+public class que5_1 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner (System.in);
+        System.out.print("Enter your marks");
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int avg=(num1+num2)/2;
+        System.out.print("Avg marks are: "+ avg);
+
+
+
+    }
+}
