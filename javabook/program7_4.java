0b5221a4ccc21918a90137b000329e4e1d6bf7c3
Sanskar Sinha <sanskarsinhanew@gmail.com>
2026-01-29 18:07:47 +0530

    right angle triangle

---
+++ javabook/program7_4.java
@@ -0,0 +1,16 @@
+//Right triangle of @
+package javabook;
+import java.util.*;
+public class program7_4 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter n: ");
+        int n=sc.nextInt();
+        for (int i=0; i<=n;i++){
+             for(int j=0;j<=i;j++){
+                System.out.print("@");
+             }
+             System.out.println();
+        }
+    }
+}
