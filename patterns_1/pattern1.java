e2ecb53eb4a248cb08ae659b7496d3895de56983
Sanskar Sinha <sanskarsinhanew@gmail.com>
2026-01-30 13:34:53 +0530

    patterns

---
+++ patterns_1/pattern1.java
@@ -0,0 +1,9 @@
+package patterns_1;
+import java.util.*;
+public class pattern1 {
+    public static void main (String args[]){
+        for(int i=0;i<=4;i++){
+            System.out.println("****");
+        }
+    }
+}
