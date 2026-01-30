4bb264ee9e099f1abe9386ddab046b71c7d09f34
Sanskar Sinha <sanskarsinhanew@gmail.com>
2026-01-30 13:41:05 +0530

    patterns

---
+++ patterns/pattern2.java
@@ -0,0 +1,24 @@
+/*
+
+*
+**
+***
+****
+
+*/
+
+package patterns;
+import java.util.*;
+public class pattern2 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter number of rows to print * ");
+        int rows=sc.nextInt();
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
