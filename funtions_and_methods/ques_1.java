ed947f6013d0f11346f2254e9ab9bdcda8da384b
Sanskar Sinha <sanskarsinhanew@gmail.com>
2026-02-01 22:18:24 +0530

    sum of to numbers

---
+++ funtions_and_methods/ques_1.java
@@ -0,0 +1,15 @@
+// write a java method to add two numbers.
+
+
+package funtions_and_methods;
+
+public class ques_1 {
+    public static int sum (int a,int b){
+        int answer=a+b;
+        return answer;
+    }
+    public static void main(String args[]){
+        System.out.print(sum(2,5));
+        
+    }
+}
