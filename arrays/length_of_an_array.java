fe6268415d5c6b7c6df9f6ec77cde1c55b753069
Sanskar Sinha <sanskarsinhanew@gmail.com>
2026-02-02 23:09:25 +0530

    Updation and length of an array

---
+++ arrays/length_of_an_array.java
@@ -0,0 +1,9 @@
+package arrays;
+
+public class length_of_an_array {
+    public static void main(String args[]){
+        int marks[]=new int [50];
+        System.out.print("Length of the array marks= "+marks.length);
+        
+    }
+}
---
+++ arrays/updation.java
@@ -0,0 +1,21 @@
+package arrays;
+import java.util.*;
+
+public class updation {
+    public static void main (String args[]){
+        int marks[]=new int[50];
+        Scanner sc=new Scanner (System.in);
+        
+        marks[0]=sc.nextInt();
+        marks[1]=sc.nextInt();
+        marks[2]=sc.nextInt();
+
+        System.out.println("Subject 1: "+marks[0]);
+        System.out.println("Subject 2: "+marks[1]);
+        System.out.println("Subject 3: "+marks[2]);
+
+        marks[2]=100;
+        System.out.println("Subject 2:"+marks[2]);
+
+    }
+}
